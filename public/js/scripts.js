ddb227f7fde1814a6c9b0ef38921cfdd54189d1f
Begopa <philip0813@naver.com>
2023-04-13 17:48:28 +0900

    사용자들끼리 서로 채팅할 수 있는 기능 추가

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,8 +8,25 @@ const formElement = getElementById('chat_form');
 
 //* global socket handler
 socket.on('user_connected', (username) => {
-  console.log(`${username} connected!`);
+  drawNewChat(`${username} connected!`);
 });
+
+socket.on('new_chat', (data) => {
+  const { chat, username } = data;
+  drawNewChat(`${username}: ${chat}`);
+});
+
+//* event callback functions
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const inputValue = event.target.elements[0].value;
+  if (inputValue !== '') {
+    socket.emit('submit_chat', inputValue);
+    // 화면에 그리기
+    drawNewChat(`me: ${inputValue}`);
+    event.target.elements[0].value = '';
+  }
+};
 
 //* draw functions
 const drawHelloStranger = (username) =>
@@ -34,6 +51,8 @@ function helloUser() {
 
 function init() {
   helloUser();
+  // 이벤트 연결
+  formElement.addEventListener('submit', handleSubmit);
 }
 
 init();
